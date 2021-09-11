4cd56800ac1515613b4b60c3466b617a9f38cd61
stkirk <shaanekirk@gmail.com>
2021-09-11 13:40:43 -0400

    logout functional, removes token from localStoarge and directs back to login page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+} from "react-router-dom";
 
 import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 import BubblePage from "./components/BubblePage";
 import "./styles.scss";
+import axios from "axios";
 
 function App() {
+  // console.log("window: ", window.location);
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    // window.location.href = "http://localhost:3000/login";
+    console.log("logging out");
+    // axios
+    //   .post("http://localhost:5000/api/logout")
+    //   .then((res) => {
+    //     console.log("Logout res: ", res);
+    //   })
+    //   .catch((err) => {
+    //     console.log("Logout err: ", err);
+    //   });
+  };
+
   return (
     <Router>
       <div className="App">
         <header>
           Color Picker Sprint Challenge
-          <a data-testid="logoutButton" href="#">
+          <a
+            data-testid="logoutButton"
+            onClick={logout}
+            href="http://localhost:3000/login"
+          >
             logout
           </a>
         </header>
